8ad4295d4d73ed6f9b5288ea0262ed7029a12fe2
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-10-07 21:16:50 -0400

    Collapse/expand field checkboxes works.

--- resources/public/js/sisyphus.js
+++ resources/public/js/sisyphus.js
@@ -3,6 +3,6 @@ jQuery(document).ready(function() {
     jQuery(".fields_checkboxes_header").click(
         function()
         {
-            jQuery(this).children().slideToggle(500);
+            jQuery(this).parent().parent().next().slideToggle(500);
         });
 });
--- src/sisyphus/views/details.clj
+++ src/sisyphus/views/details.clj
@@ -67,16 +67,17 @@
    (hidden-field :id (:_id run))
    (hidden-field :comparative (if comparative? "true" "false"))
    (hidden-field :problem (:problem run))
-   [:div.fields_checkboxes_header "Click me"
-    [:div.fields_checkboxes
-     [:div.row
-      [:div.span4.columns [:h3 "Active fields"]]
-      (field-checkboxes run comparative? :fields fields)]
-     [:div.row
-      [:div.span4.columns "&nbsp;"]
-      [:div.span12.columns
-       [:div.actions
-        [:input.btn.primary {:value "Update" :type "submit"}]]]]]]))
+   [:div.row
+    [:div.span4.columns [:h3.fields_checkboxes_header "Select active fields..."]]]
+   [:div.fields_checkboxes
+    [:div.row
+     [:div.span4.columns "&nbsp;"]
+     (field-checkboxes run comparative? :fields fields)]
+    [:div.row
+     [:div.span4.columns "&nbsp;"]
+     [:div.span12.columns
+      [:div.actions
+       [:input.btn.primary {:value "Update" :type "submit"}]]]]]))
 
 (defn filter-on-fields
   [problem results-type fields]
