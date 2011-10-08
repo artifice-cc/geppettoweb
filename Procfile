51a5ad11bce49b2a5ae28d53d64d5404ebdf5114
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-10-07 21:50:28 -0400

    Trying to fix heroku.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: lein run -m sisysphus.server
+web: lein run
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject sisyphus "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [noir "1.1.1-SNAPSHOT"]
+                 [noir "1.2.0"]
                  [com.ashafa/clutch "0.2.4"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
   :dev-dependencies [[swank-clojure "1.3.0"]
