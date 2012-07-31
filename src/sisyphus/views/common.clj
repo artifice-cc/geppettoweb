640a7db5d9a4a2f69d06e7b6b7420f88b3702b10
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-07-30 22:58:22 -0400

    Updated styling of sidebar links.

--- src/sisyphus/views/common.clj
+++ src/sisyphus/views/common.clj
@@ -44,7 +44,7 @@
            (map (fn [[_ anchor ds title]]
                   (let [l (link-to (format "#%s" anchor) title)
                         d (Integer/parseInt ds)]
-                    [:div (if (= d 2) [:b l] l)])) headers)]])
+                    [:div (cond (= d 1) [:b [:i l]] (= d 2) [:b l] :else l)])) headers)]])
        [:div.content {:style "position: relative; top: 50px;"}
         chtml]]])))
 
