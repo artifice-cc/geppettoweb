7cdf41287da21b5b5753d4b32ae6e493b2e57515
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-01-28 11:57:57 -0500

    Bars template graph supports facets.

--- templates/graph_template_bars.r
+++ templates/graph_template_bars.r
@@ -1,13 +1,32 @@
 m <- merge(control, comparison, by=c("simulation"))
 result <- data.frame(value=c(), se=c(),
                      <(str (if (:fill graph) "fill=c()," ""))>
+                     <(str (if (:color graph) "color=c()," ""))>
                      xfactor=c())
+
 <(if (:fill graph) ">
 for(fill in unique(m$<(:fill graph)>.y)) {
 <")>
+
+<(if (:color graph) ">
+for(color in unique(m$<(:color graph)>.y)) {
+<")>
+
+<(if (:facethoriz graph) ">
+for(facethoriz in unique(m$<(:facethoriz graph)>.y)) {
+<")>
+
+<(if (:facetvert graph) ">
+for(facetvert in unique(m$<(:facetvert graph)>.y)) {
+<")>
+
 for(xfactor in unique(m$<(:xfactor graph)>.y)) {
+
   data <- subset(m,
                  <(if (:fill graph) "><(:fill graph)>.y == fill & <")>
+                 <(if (:color graph) "><(:color graph)>.y == color & <")>
+                 <(if (:facethoriz graph) "><(:facethoriz graph)>.y == facethoriz & <")>
+                 <(if (:facetvert graph) "><(:facetvert graph)>.y == facetvert & <")>
                  <(:xfactor graph)>.y == xfactor)
 
   if(nrow(data) > 0) {
@@ -16,17 +35,32 @@ for(xfactor in unique(m$<(:xfactor graph)>.y)) {
     dse <- summarySE(d, measurevar="value", groupvars=c("label"))
     result <- rbind(result, data.frame(
                               <(str (if (:fill graph) "fill=fill," ""))>
+                              <(str (if (:color graph) "color=color," ""))>
+                              <(str (if (:facethoriz graph) "facethoriz=facethoriz," ""))>
+                              <(str (if (:facetvert graph) "facetvert=facetvert," ""))>
                               xfactor=xfactor,
                               value=dse$value, se=dse$se))
   }
 }
 <(str (if (:fill graph) "}" ""))>
+<(str (if (:color graph) "}" ""))>
+<(str (if (:facethoriz graph) "}" ""))>
+<(str (if (:facetvert graph) "}" ""))>
 
 p <- ggplot(result, aes(x=factor(xfactor), y=value,
-                        <(str (if (:fill graph) "fill=factor(fill)," ""))>))
+                        <(str (if (:fill graph) "fill=factor(fill)," ""))>
+                        <(str (if (:color graph) "color=factor(color)," ""))>
+                        ))
 p <- p + geom_bar(position=position_dodge())
 p <- p + geom_errorbar(aes(ymin=value-se, ymax=value+se),
                        width=.5, position=position_dodge(.9))
-p <- p + scale_fill_grey()
 p <- p + scale_x_discrete("<(:xlabel graph)>")
 p <- p + scale_y_continuous("<(:ylabel graph)>")
+
+<(if (or (:facethoriz graph) (:facetvert graph)) ">
+p <- p + facet_grid(
+<(str (if (:facetvert graph) "facetvert" "."))>
+~
+<(str (if (:facethoriz graph) "facethoriz" "."))>
+)
+<")>
